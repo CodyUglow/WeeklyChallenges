6eeda746bd62723f672874d660dac07ecd2b4dc7
CodyUglow <blackheart3621@gmail.com>
2025-08-06 15:37:12 -0500

    Completed first challenge set as instructed.

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -18,32 +18,53 @@ namespace ChallengesWithTestsMark8
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            double result = minuend - subtrahend;
+            return result;
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            int result = number1 + number2;
+            return result;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            if (number1 < number2)
+            {
+                return number1;
+            }
+            else
+            {
+                return number2;
+            }
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            long result = factor1 * factor2;
+            return result;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+           Console.WriteLine ("Welcome to the program, please enter your name :");
+           string greeting = Console.ReadLine();
+            greeting = "Hello " + nameOfPerson;
+            return greeting;
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            var username = Console.ReadLine();
+            if (string.IsNullOrEmpty(username))
+            {
+                return "Hey!";
+            }
+            else
+            {
+                return "Hey " + username + "!";
+            }
         }
     }
 }
